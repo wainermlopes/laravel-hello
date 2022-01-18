585bda8d85c5bdc5f50c09b4b04e3683184920dd
wainer <wainer@wbcsistemas.com.br>
2022-01-17 21:49:44 -0300

    exercicio controler

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloControler;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/mazza',function(){
+  return 'MazzaFC';
+});
+*/
+Route::get('/', [HelloControler::class, 'index']);#
